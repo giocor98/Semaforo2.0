1e4d24632f1096a848e4da4c6707cccf31c0cb4a
GioCor <giorgiocor98@gmail.com>
2020-07-15 14:33:41 +0200

    fixed pom for tests

---
+++ src/test/java/src/app/MainAppTest.java
@@ -0,0 +1,14 @@
+package src.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainAppTest {
+
+    @Test
+    void main() {
+        assert(true);
+        //fail();
+    }
+}
